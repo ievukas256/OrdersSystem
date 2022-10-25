25eacd4bd6320015b25792cab7aea1aa92d604b5
Your Nameievukas256 <you@example.comsibailaite@gmail.com>
2022-10-25 13:05:15 +0300

    create add and remove metods to products list

--- Program.cs
+++ Program.cs
@@ -1,66 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-using OrdersSystem;
+﻿using OrdersSystem;
+using OrdersSystem.Classes;
+using OrdersSystem.Reports;
 using OrdersSystem.Repositories;
+using System.Diagnostics;
+using System.Xml.Linq;
 
 CustomersRepository customersRepository = new CustomersRepository();
 OrdersRepository ordersRepository = new OrdersRepository();
 ProductsRepository productsRepository = new ProductsRepository();
 
 ReportGenerator reportGenerator = new ReportGenerator(customersRepository, ordersRepository, productsRepository);
-/*var result = reportGenerator.GenerateNewOrdersByCustomers();
-foreach (var item in result)
-{
-    Console.WriteLine(item.OrderNumber);
-    Console.WriteLine(item.FullName);
-    Console.WriteLine(item.OrderStatus);
-    
-}*/
-
-var result2 = reportGenerator.GenerateCustomersByBoughtProducts();
-foreach (var item in result2)
-{
-    Console.WriteLine(item.OrderNumber);
-    Console.WriteLine(item.FullName);
-    Console.WriteLine(item.ProductName);
-
-}
 
 bool working = true;
 
 while (working)
 {
-    Console.WriteLine("Pasirinkite ataskaita: [1]Neapmokėti užsakymai [2]Klientų ataskaita [3]Išeiti");
-    int input = int.Parse(Console.ReadLine());
+	Console.WriteLine("[1]Neapmokėti užsakymai [2]Klientų ataskaita [3]Išeiti \n[4]Prideti produkta [5]Pasalinti produkta [6]Produktu sarasas");
+	int input = int.Parse(Console.ReadLine());
 
-    switch(input)
-    {
-        case 1:
-            {
-                var result = reportGenerator.GenerateNewOrdersByCustomers();
-                foreach (var item in result)
-                {
-                    Console.WriteLine(item.OrderNumber);
-                    Console.WriteLine(item.FullName);
-                    Console.WriteLine(item.OrderStatus);
-                }
-                break;
-            }
-        case 2:
-            {
-                var result1 = reportGenerator.GenerateCustomersByBoughtProducts();
-                foreach (var item in result1)
-                {
-                    Console.WriteLine(item.OrderNumber);
-                    Console.WriteLine(item.FullName);
-                    Console.WriteLine(item.ProductName);
-                }
-                break;
-            }
-        case 3:
-            {
-                Console.WriteLine("Viso gero!");
-                working = false;
-                break;
-            }
-    }
+	switch(input)
+	{
+		case 1:
+			{
+				var result = reportGenerator.GenerateNewOrdersByCustomers();
+				foreach (var item in result)
+				{
+					Console.Write("Uzsakymo numeris:");
+					Console.WriteLine(item.OrderNumber);
+					Console.Write("Uzsakymo statusas:");
+					Console.WriteLine(item.OrderStatus);
+					Console.Write("Klientas:");
+					Console.WriteLine(item.FullName);
+				}
+				break;
+			}
+		case 2:
+			{
+				var result1 = reportGenerator.GenerateCustomersByBoughtProductsOver100();
+				foreach (var item in result1)
+				{
+					Console.Write("Uzsakymo numeris:");
+					Console.WriteLine(item.OrderNumber);
+					Console.Write("Kliento numeris:");
+					Console.WriteLine(item.CustomerID);
+					Console.Write("Produkto pavadinimas:");
+					Console.WriteLine(item.ProductName);
+				}
+				break;
+			}
+		case 3:
+			{
+				Console.WriteLine("Viso gero!");
+				working = false;
+				break;
+			}
+		case 4:
+			{
+				productsRepository.AddProduct();
+				break;
+			}
+		case 5:
+			{
+				productsRepository.DeleteProduct();
+				break;
+			}
+		case 6:
+			{
+				productsRepository.GetAllProducts();
+				break;
+			}
+		default:
+			{
+				Console.WriteLine("Tokio meniu punkto nera");
+				break;
+			}
+	}
 }
+
+/*JSONgenerator jsonGenerator = new JSONgenerator(reportGenerator);
+var jsonResult = jsonGenerator.GenerateJsonWithReports();*/
+
+HTMLgenerator htmlGenerator = new HTMLgenerator(reportGenerator);
+var htmlResult = htmlGenerator.GenerateHtmlReportOrders();
--- Repositories/ProductsRepository.cs
+++ Repositories/ProductsRepository.cs
@@ -1,33 +1,97 @@
-﻿using OrdersSystem.Classes;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OrdersSystem.Classes;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
 namespace OrdersSystem.Repositories
 {
-    public class ProductsRepository
-    {
-        private List<Products> products { get; set; } = new List<Products>();
-        public ProductsRepository()
-        {
-            products.Add(new Products(2001, "Stalas", 105.50, 2));
-            products.Add(new Products(2002, "Biuro kede", 55, 5));
-            products.Add(new Products(2003, "Komoda", 150.60, 10));
-            products.Add(new Products(2004, "Spintele", 25, 23));
-            products.Add(new Products(2005, "Popierius", 5.50, 99));
-            products.Add(new Products(2006, "Spausdintuvas", 250, 7));
-            products.Add(new Products(2007, "Monitorius", 450, 0));
-        }
+	public class ProductsRepository
+	{
+		private List<Products> products { get; set; } = new List<Products>();
+		public ProductsRepository()
+		{
+		   /* products.Add(new Products(2001, "Stalas", 105.50, 2));
+			products.Add(new Products(2002, "Biuro kede", 55, 5));
+			products.Add(new Products(2003, "Komoda", 150.60, 10));
+			products.Add(new Products(2004, "Spintele", 25, 23));
+			products.Add(new Products(2005, "Popierius", 5.50, 99));
+			products.Add(new Products(2006, "Spausdintuvas", 250, 7));
+			products.Add(new Products(2007, "Monitorius", 450, 0));*/
 
-        public List<Products> Retrieve()
-        {
-            return products;
-        }
-        public Products Retrieve(int productId)
-        {
-            return products.SingleOrDefault(x => x.ProductID == productId);
-        }
-    }
+			using (StreamReader r = new StreamReader("C:\\Users\\sibai\\Desktop\\mokslai\\Visual studio\\OrdersSystem\\DataFiles\\ProductsData.json"))
+			{
+				string json = r.ReadToEnd(); 
+				products = JsonConvert.DeserializeObject<List<Products>>(json);
+			} 
+		}
+		public List<Products> Retrieve()
+		{
+			return products;
+		}
+		public Products Retrieve(int productId)
+		{
+			return products.FirstOrDefault(x => x.ProductID == productId);
+		}
+		public void GetAllProducts()
+		{
+			foreach(Products item in products)
+			{
+				Console.WriteLine($"ProductID: {item.ProductID}");
+				Console.WriteLine($"ProductName: {item.ProductName}");
+				Console.WriteLine($"ProductPrice: {item.ProductPrice}");
+				Console.WriteLine($"ProductQuantity: {item.ProductQuantity}");
+				Console.WriteLine("------------------");
+			}
+		}
+		public void Save(Products entity)
+		{
+			products.Add(entity);
+		}
+		public void AddProduct()
+		{
+			Console.WriteLine("Iveskite produkto pavadinima:");
+			string name = Console.ReadLine();
+			Console.WriteLine("Iveskite produkto kaina:");
+			double price = int.Parse(Console.ReadLine());
+			Console.WriteLine("Iveskite produkto kieki:");
+			int quant = int.Parse(Console.ReadLine());
+			int id = products.Max(x => x.ProductID);
+			int id2 = id + 1;
+
+			var newProduct = new Products(id2, name, price, quant);
+			products.Add(newProduct);
+
+			string path = @"C:\\Users\\sibai\\Desktop\\mokslai\\Visual studio\\OrdersSystem\\DataFiles\\ProductsData.json";
+			var jsonString = File.ReadAllText(path);
+			var list = JsonConvert.DeserializeObject<List<Products>>(jsonString);
+			list.Add(newProduct);
+			var convertedJson = JsonConvert.SerializeObject(list, Formatting.Indented);
+			File.WriteAllText(path, convertedJson);
+
+			Console.WriteLine($"Sukurtas naujas produktas: {id2} {name} {price} {quant}");
+		}
+		public void DeleteProduct()
+		{
+			Console.WriteLine("Iveskite produkto ID kuri norite pasalinti");
+			int productId = int.Parse(Console.ReadLine());
+			var item = products.SingleOrDefault(x => x.ProductID == productId);
+			products.Remove(item);
+
+            string path = @"C:\\Users\\sibai\\Desktop\\mokslai\\Visual studio\\OrdersSystem\\DataFiles\\ProductsData.json";
+            var jsonString = File.ReadAllText(path);
+            var list = JsonConvert.DeserializeObject<List<Products>>(jsonString);
+            list.Remove(item);
+            var convertedJson = JsonConvert.SerializeObject(list, Formatting.Indented);
+            File.WriteAllText(path, convertedJson);
+			Console.WriteLine("Produktas pasalintas");
+		}
+	}
 }
